23f8ad19b89871924aa3f985a2da436f3f1a12fb
Rich Boyce <122512687+rboyce212@users.noreply.github.com>
2023-10-26 15:24:25 -0400

    added readme

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+// http request (frontend, fetch(http:3001)) -> server.js
+// -> app.js (figures out where to route)
+// -> controller (handling the logic for the response)
+
 // DEPENDENCIES
 const app = require("./app.js");
 
